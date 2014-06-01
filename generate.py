210abfd6143d2af3c4fad0966cf1b44c06863ba2
Marcin Zdun <mzdun@midnightbits.com>
2014-06-01 16:02:34 +0200

    More help info

--- generate.py
+++ generate.py
@@ -2,13 +2,13 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", help="Name of the plugin", required=True)
-parser.add_argument("--mime-type", help="MIME type to support", required=True)
-parser.add_argument("--descr", help="Plugin description", default="")
-parser.add_argument("--version", help="Plugin version", default="1.0.0")
+parser.add_argument("--mime-type", metavar="TYPE", help="MIME type to support. Type should be application private (Should start with \"application/x-\")", required=True)
+parser.add_argument("--descr", help="Plugin description (default: empty)", default="")
+parser.add_argument("--version", help="Plugin version (default: 1.0.0)", default="1.0.0")
 parser.add_argument("--namespace", metavar="CPP-NS", help="Code namespace (default: based on plugin name)")
 parser.add_argument("--target-name", metavar="NAME", help="DLL name (default: same as namespace)")
-parser.add_argument("--instance", metavar="CLASS", help="Class to use for the instance", default="Instance")
-parser.add_argument("--module", metavar="CLASS", help="Class to use for the module", default="Module")
+parser.add_argument("--instance", metavar="CLASS", help="Class to use for the instance (default: Instance)", default="Instance")
+parser.add_argument("--module", metavar="CLASS", help="Class to use for the module (default: Module)", default="Module")
 args = parser.parse_args()
 print(args)
 
