8e2601a17762da5905702338e12a1cd6c188cbbd
Marcin Zdun <mzdun@midnightbits.com>
2014-06-01 15:15:11 +0200

    Initial argparse setup

--- generate.py
+++ generate.py
@@ -1,15 +1,23 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--name", help="Name of the plugin", required=True)
+parser.add_argument("--mime-type", help="MIME type to support", required=True)
+parser.add_argument("--descr", help="Plugin description", default="")
+parser.add_argument("--version", help="Plugin version", default="1.0.0")
+parser.add_argument("--namespace", metavar="CPP-NS", help="Code namespace (default: based on plugin name)")
+parser.add_argument("--target-name", metavar="NAME", help="DLL name (default: same as namespace)")
+parser.add_argument("--instance", metavar="CLASS", help="Class to use for the instance", default="Instance")
+parser.add_argument("--module", metavar="CLASS", help="Class to use for the module", default="Module")
 args = parser.parse_args()
 print(args)
 
 #PLUGIN_NAME == ?
 #PLUGIN_MIMETYPE = ?
-#PLUGIN_TARGET == PLUGIN_NAME -> camel to gtk
 #PLUGIN_DESCRIPTION == ""
 #PLUGIN_VERSION = 1.0.0
-#PLUGIN_NAMESPACE == PLUGIN_TARGET
+#PLUGIN_NAMESPACE == PLUGIN_NAME -> camel to gtk
+#PLUGIN_TARGET == PLUGIN_NAMESPACE
 #INSTANCE_CLASS == Instance
 #MODULE_CLASS == Module
 #YEAR == today().year
