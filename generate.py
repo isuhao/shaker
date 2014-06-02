01bf6fbc176907e73acadc0309d920a12c4e9881
Marcin Zdun <mzdun@midnightbits.com>
2014-06-02 16:43:46 +0200

    Simple template engine

--- generate.py
+++ generate.py
@@ -1,4 +1,14 @@
-import argparse
+import argparse, re, os, uuid, tmplt
+from datetime import date
+
+def convert(name):
+	s1 = re.sub('(.)\s(.)', r'\1_\2', name)
+	s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', s1)
+	s1 = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+	return re.sub('_+', '_', s1)
+
+def decamelize(unsafe):
+	return unsafe.replace(" ", "_")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", help="Name of the plugin", required=True)
@@ -9,8 +19,36 @@ parser.add_argument("--namespace", metavar="CPP-NS", help="Code namespace (defau
 parser.add_argument("--target-name", metavar="NAME", help="DLL name (default: same as namespace)")
 parser.add_argument("--instance", metavar="CLASS", help="Class to use for the instance (default: Instance)", default="Instance")
 parser.add_argument("--module", metavar="CLASS", help="Class to use for the module (default: Module)", default="Module")
+parser.add_argument("--solution", action="store_true", help="If present, will create solution with a project")
+parser.add_argument("--solution-name", metavar="SLN", help="Name of the solution (default: same as target)")
+
 args = parser.parse_args()
-print(args)
+if args.namespace is None:
+	args.namespace = convert(args.name)
+
+if args.target_name is None:
+	args.target_name = args.namespace
+
+if args.solution_name is None:
+	args.solution_name = args.target_name
+
+vars = {}
+vars['PLUGIN_NAME'] = args.name
+vars['PLUGIN_MIMETYPE'] = args.mime_type
+vars['PLUGIN_DESCRIPTION'] = args.descr
+vars['PLUGIN_VERSION'] = args.version
+vars['PLUGIN_NAMESPACE'] = args.namespace
+vars['PLUGIN_TARGET'] = args.target_name
+vars['INSTANCE_CLASS'] = args.instance
+vars['MODULE_CLASS'] = args.module
+if args.solution:
+	vars['SOLUTION'] = args.solution_name
+vars['YEAR'] = date.today().year
+vars['PROJ_GUID'] = str(uuid.uuid1()).upper()
+vars['SHAKER_HOME'] = os.path.dirname(os.path.realpath(__file__))
+
+t = tmplt.Template(vars, "default")
+t.install()
 
 #PLUGIN_NAME == ?
 #PLUGIN_MIMETYPE = ?
---
+++ tmplt.py
@@ -0,0 +1,189 @@
+import os, sys
+
+TEXT     = 0
+VARIABLE = 1
+IF       = 2
+ELIF     = 3
+ELSE     = 4
+ENDIF    = 5
+FUNC     = 6
+
+def pack_ifs(code, index):
+	out = []
+	pos = index
+	length = len(code)
+	restart = False
+	while pos < length:
+		item = code[pos]
+		if item[0] == IF:
+			part, pos, restart = pack_ifs(code, pos + 1)
+			out.append((IF, item[1], part))
+			continue
+		elif item[0] == ELIF:
+			if restart:
+				part, pos, restart = pack_ifs(code, pos + 1)
+				out.append((ELSE, item[1], part))
+				continue
+			else:
+				return (out, pos, True)
+		elif item[0] == ELSE:
+			if restart:
+				part, pos, restart = pack_ifs(code, pos + 1)
+				out.append((ELSE, None, part))
+				continue
+			else:
+				return (out, pos, True)
+		elif item[0] == ENDIF:
+			return (out, pos + 1, True)
+		elif item[0] == TEXT:
+			#print pos, index, restart, "%r" % item[1]
+			if (pos == index and index > 0) or restart: out.append((TEXT, item[1].lstrip("\n\r")))
+			else: out.append(item)
+		else:
+			out.append(item)
+		restart = False
+		pos += 1
+
+	return (out, pos, False)
+
+def print_code(code, indent = ""):
+	for item in code:
+		if item[0] == IF:
+			print "%sif %s:" % (indent, item[1])
+			print_code(item[2], indent + "    ")
+		elif item[0] == ELIF:
+			print "%selif %s:" % (indent, item[1])
+			print_code(item[2], indent + "    ")
+		elif item[0] == ELSE:
+			print "%selse:" % indent
+			print_code(item[2], indent + "    ")
+		elif item[0] == TEXT:
+			print "%s%r" % (indent, item[1])
+		elif item[0] == VARIABLE:
+			print "%s%%[[%s]]" % (indent, item[1])
+		elif item[0] == FUNC:
+			print "%s%%%s(%s)" % (indent, item[1], item[2])
+		else:
+			print "%s%r" % (indent, item)
+
+def stringify(arg):
+	return '"' + arg.replace("\\", "\\\\").replace('"', '\\"') + '"'
+
+class Template:
+	def __init__(self, vars, name):
+		self.name = name
+		self.vars = vars
+		self.files = {}
+		self.outdir = os.path.realpath(self.vars["PLUGIN_TARGET"])
+		if "SOLUTION" in self.vars:
+			self.outdir = os.path.realpath(self.vars["SOLUTION"])
+		self.source = os.path.join(os.path.dirname(os.path.realpath(__file__)), "template", name)
+		files = self.compile(os.path.join(self.source, name + ".files"))
+		text = self.produce(files)
+		for line in text.split('\n'):
+			line = line.split('=', 1)
+			if len(line) < 2: continue
+			self.files[line[0].strip()] = line[1].strip()
+
+	def compile(self, path):
+		try:
+			f = open(path, "r")
+		except IOError as e:
+			print "%s: error %s: %s" % (path, e.errno, e.strerror)
+			sys.exit(1)
+		src = f.read()
+		f.close()
+		src = src.split("%[[")
+		out = []
+		out.append((TEXT, src[0]))
+		for i in range(1, len(src)):
+			s = src[i].split("]]")
+			if len(s) != 2:
+				print "%s: error: bad syntax" % path
+				sys.exit(1)
+			v = s[0]
+			if v[:3] == "if:":
+				out.append((IF, v[3:]))
+			elif v[:5] == "elif:":
+				out.append((ELIF, v[5:]))
+			elif v == "else":
+				out.append((ELSE, None))
+			elif v == "endif":
+				out.append((ENDIF, None))
+			elif ':' in v:
+				v = v.split(':', 1)
+				out.append((FUNC, v[0], v[1]))
+			else:
+				out.append((VARIABLE, v))
+			out.append((TEXT, s[1]))
+			
+		out, index, ignore = pack_ifs(out, 0)
+		return out
+
+	def produce(self, compiled):
+		#print_code(compiled)
+		return "".join(self.run(compiled))
+
+	def _if(self, var):
+		if var not in self.vars: return False
+		return not not self.vars[var]
+
+	def run(self, code):
+		skipping = False
+		out = []
+
+		for item in code:
+			if item[0] == IF:
+				skipping = self._if(item[1])
+				if skipping:
+					sub = self.run(item[2])
+					out += sub
+			elif item[0] == ELIF:
+				if skipping: continue
+				skipping = self._if(item[1])
+				if skipping:
+					sub = self.run(item[2])
+					out += sub
+			elif item[0] == ELSE:
+				if skipping: continue
+				sub = self.run(item[2])
+				out += sub
+			elif item[0] == TEXT:
+				out.append(item[1])
+			elif item[0] == VARIABLE:
+				if item[1] in self.vars:
+					out.append(str(self.vars[item[1]]))
+			elif item[0] == FUNC:
+				if item[1] == "STR":
+					if item[2] in self.vars:
+						out.append(stringify(str(self.vars[item[2]])))
+				else:
+					print "error: unknown function `%s'" % item[1]
+					sys.exit(1)
+			else:
+				print "error: unknown opcode %s" % item[0]
+				sys.exit(1)
+
+		return out
+		
+	def install(self):
+		keys = self.files.keys()
+		keys.sort()
+		for infile in keys:
+			#print os.path.join(self.source, infile), "->", os.path.join(self.outdir, self.files[infile])
+			code = self.compile(os.path.join(self.source, infile))
+			text = self.produce(code)
+			
+			self.output(text, os.path.join(self.outdir, self.files[infile]))
+			
+	def output(self, text, path):
+		try:
+			directory = os.path.dirname(path)
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+			f = open(path, "w")
+			f.write(text)
+			f.close()
+		except IOError as e:
+			print "%s: error %s: %s" % (path, e.errno, e.strerror)
+			sys.exit(1)
